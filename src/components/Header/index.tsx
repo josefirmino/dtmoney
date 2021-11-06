211bc087ec535b1b07c1c1c5f7cd6d1c0c4b5418
josefirmino <jose.theangelz@gmail.com>
2021-11-06 12:31:02 -0300

    Criando componente de Modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,13 +3,19 @@ import logoImg from "../../assets/logo.svg";
 // Styled
 import { Section, Container, HeaderDiv } from "./styles";
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransitionModal: () => void;
+}
+
+export function Header({ onOpenNewTransitionModal }: HeaderProps) {
   return (
     <Section>
       <Container>
         <HeaderDiv>
           <img src={logoImg} alt="dt money" />
-          <button type="button">Nova transação</button>
+          <button type="button" onClick={onOpenNewTransitionModal}>
+            Nova transação
+          </button>
         </HeaderDiv>
       </Container>
     </Section>
